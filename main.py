cce691b120f4458ba6f396de48a0b5824179861b
salicat <bbscarlos@hotmail.com>
2022-05-18 15:29:36 +0200

    CORS git-pages

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ origins = [
     "http://localhost:8080", 
     "http://localhost:8000", 
     "https://mybudgetvue.herokuapp.com",
-    "https://6284ce8a8e5b226bcdd4a2c2--cosmic-sfogliatella-cf7c4c.netlify.app/"
+    "https://salicat.github.io/my_budget_vue/"
 ]
 
 my_budget.add_middleware(
