de3c5ac01cecf7f195d80032749f0623d1070a5d
salicat <bbscarlos@hotmail.com>
2022-05-20 15:20:02 +0200

    CORS ["*"]

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = [
 
 my_budget.add_middleware(
     CORSMiddleware, 
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True, 
     allow_methods=["*"], 
     allow_headers=[],
