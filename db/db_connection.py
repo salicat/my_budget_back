320bdd4784fee1954c058315f6b5e0cc5fdc5d4f
salicat <bbscarlos@hotmail.com>
2022-06-08 18:20:01 +0200

    Update db_connection.py

--- db/db_connection.py
+++ db/db_connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://uwwqomxcjzyenm:a0b4c1376dfbf47fc44c966d99f476a728f947a85571f6af23c39fa8f831812a@ec2-52-204-195-41.compute-1.amazonaws.com:5432/d2jt20o0r9kcas"
+DATABASE_URL = "postgresql://mvmltkesjxfeun:787a58ec4497befcfc2626b8dfd1032ead98b553032df74dc4780d736b33e9fb@ec2-52-72-99-110.compute-1.amazonaws.com:5432/d4d3chl5l23csp"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, 
