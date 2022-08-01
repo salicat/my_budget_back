6c556e3a0472915c08937eeabd3f65092bc40ba0
salicat <bbscarlos@hotmail.com>
2022-08-01 10:34:48 -0500

    Update cat_router.py

--- routers/cat_router.py
+++ routers/cat_router.py
@@ -111,10 +111,13 @@ async def modify_cat(cat_update: CatUpDate, db: Session = Depends(get_db)):
     modified = []
 
     for cat in all_cats:
-        if cat_update.category == cat.category:
             if cat_update.username == cat.username:
-                cat.category = cat_update.category
                 cat.budget = cat_update.budget
+                modified.append(cat)
+                db.commit()
+                db.refresh(cat)
+            if cat_update.category != cat.category:
+                cat.category = cat_update.category
                 modified.append(cat)
                 db.commit()
                 db.refresh(cat)
