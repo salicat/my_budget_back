d117ca5c9e6d750b6e482430e79a6f544886c3a8
salicat <bbscarlos@hotmail.com>
2022-08-01 16:35:14 -0500

    Update cat_router.py

--- routers/cat_router.py
+++ routers/cat_router.py
@@ -132,9 +132,8 @@ async def delete_cat(cat_del: CatDel, db : Session = Depends(get_db)):
     for reg in user_cats:
         if cat_del.type == reg.type:
             if cat_del.username == reg.username:
-                for reg in user_cats:
-                    db.delete(reg)
-                    db.commit()
-                    db.flush(reg)
+                db.delete(reg)
+                db.commit()
+                db.flush(reg)
 
     return user_cats
